833fd2f71a6bba8e3b114a6c7bd8f14577893ace
Mammen Mathew <mammen999@gmail.com>
2022-12-08 16:57:52 +0530

    axios stuck!!!!

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,16 +1,26 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
+import{API_KEY} from './constants'
 import "./Banner.css"
-import Axios from './Axios'
+import axios from 'axios'
 
 
-function Banner() {useEffect(() => {
+function Banner() {
+  const [Movie, setMovie] = useState()
+  useEffect(() => {
+  axios.get(`/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+    console.log(response.data.results[0])
+    setMovie(response.data.results[0])
+
+  })
+  
+  
  
 }, [])
 
   return (
     <div className='banner'>
         <div className='contant'>
-            <h1 className='tiltle'>Movie Name</h1>
+            <h1 className='tiltle'>{Movie.title}</h1>
          <div className='banner_buttons'>
             <button className='buttons'>Play</button>
             <button className='buttons'>My List</button>
